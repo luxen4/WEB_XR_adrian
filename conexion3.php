6a5b43e45f19f7f5d8ebfa041431f32bded8645c
luxen4 <superlaya51@gmail.com>
2024-12-19 22:58:25 +0100

    commit135

---
+++ conexion3.php
@@ -0,0 +1,14 @@
+$servidor = "fdb1030.awardspace.net"; // Dirección del servidor de base de datos
+$usuario = "3714088_pokemon"; // Usuario de la base de datos
+$contraseña = "qMe@z:2-4uCj7DlD"; // Contraseña de la base de datos
+$nombreBD = "3714088_pokemon"; // Nombre de la base de datos
+
+// Crear la conexión
+$conexion = new mysqli($servidor, $usuario, $contraseña, $nombreBD);
+
+// Verificar la conexión
+if ($conexion->connect_error) {
+    die("Error de conexión: " . $conexion->connect_error);
+} else {
+    echo "Conexión exitosa a la base de datos.";
+}
